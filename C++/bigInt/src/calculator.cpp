8a52577b3bc3540470d82ad6ec28190b8ba4bec9
ChickenNugget-25 <peter.wiggins25@gmail.com>
2025-01-21 22:22:03 -0500

    bigint 29

--- C++/bigInt/src/calculator.cpp
+++ C++/bigInt/src/calculator.cpp
@@ -1,12 +1,32 @@
 #include "bigint.h"
+#include <iostream>
+using namespace std;
+
+enum op_codes {
+    ADD,
+    ADD_MEM,
+    SUBTRACT,
+    SUBTRACT_MEM,
+
+    EQUAL_MEM,
+    NOT_EQUAL_MEM,
+
+    GREATER_THAN_MEM,
+    GREATER_THAN_EQUAL_MEM,
+
+    LESS_THAN_MEM,
+    LESS_THAN_EQUAL_MEM
+
+};
+
 
 int main() {
-    bigint bint("1000");
-    bigint bint2(2);
+    bigint mem;
+    char in;
+    int opcode;
+    
+    while (true) {
+        cin >> in;
 
-    bint.print();
-    bint2.print();
-
-    bint -= bint2;
-    bint.print();
+    }
 }
