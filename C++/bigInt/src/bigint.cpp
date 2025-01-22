ca605d494b6c3a83eff53b8e9329329c7f937902
ChickenNugget-25 <peter.wiggins25@gmail.com>
2025-01-21 22:57:09 -0500

    bigint 36

--- C++/bigInt/src/bigint.cpp
+++ C++/bigInt/src/bigint.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 string removePadding(string str) {
 
-    for (int i = str.length() - 1; i >= 0; i--) {
+    for (int i = str.length() - 1; i > 0; i--) {
         if (str[i] != 0) {
             return str;
         }
